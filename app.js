d0e0845f5084c97ddc8ed717b53898b247afe7a9
ttac1 <81249031+ttac1@users.noreply.github.com>
2021-05-16 05:58:43 +0200

    add some file
    #3.10 까지 강의 cloning

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
+const app = express();
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
+ 
+export default app;
---
+++ routers/userRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+export const userRouter = express.Router();
+
+export default userRouter;
+
+// MVC (pattern)
+//M (modell) data
+//V (view) how does the data look
+//C (controller) function that looks for the data
