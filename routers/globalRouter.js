c3711b2e1c002426c10fc1470e772148e6b7e512
ttac1 <81249031+ttac1@users.noreply.github.com>
2021-05-17 10:38:40 +0200

    add controllers
    bis3.12

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,5 +1,14 @@
 import express from "express";
+import routes from "../routes";
+import { join, login, logout } from "../controller/userController";
+import { home, search } from "../controller/videoController";
 
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home,home);
+globalRouter.get(routes.search,search);
+globalRouter.get(routes.join,join);
+globalRouter.get(routes.login,login);
+globalRouter.get(routes.logout,logout);
 
 export default globalRouter;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,8 +1,22 @@
 import express from "express";
+import routes from "../routes";
 
-export const userRouter = express.Router();
+import {
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+} from "../controller/userController";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
+
 
 // MVC (pattern)
 //M (modell) data
